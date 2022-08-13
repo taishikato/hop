6f0f4bac9f88581bf2add5f54b6c80fafb2ac64e
taishikato <taishi.k0903@gmail.com>
2022-08-13 15:00:27 -0700

    set a default mode as a dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@
 import "./index.css";
 import { render } from "solid-js/web";
 import { Router } from "@solidjs/router";
+import { HopeProvider, HopeThemeConfig } from "@hope-ui/solid";
 
 import App from "./App";
+
+const config: HopeThemeConfig = {
+  initialColorMode: "dark",
+};
 
 render(
   () => (
     <Router>
-      <App />
+      <HopeProvider config={config}>
+        <App />
+      </HopeProvider>
     </Router>
   ),
   document.getElementById("root") as HTMLElement
