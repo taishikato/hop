3c2a08f8e33702a3b1e65e3bb411caeceea8bdc7
taishikato <taishi.k0903@gmail.com>
2022-08-16 19:07:54 -0700

    create skills.ts

---
+++ src/consts/skills.ts
@@ -0,0 +1,34 @@
+export const skills = [
+  {
+    title: "JavaScript",
+    value: "javascript",
+  },
+  {
+    title: "TypeScript",
+    value: "typescript",
+  },
+  {
+    title: "React",
+    value: "react",
+  },
+  {
+    title: "Next.js",
+    value: "next",
+  },
+  {
+    title: "Vue.js",
+    value: "vue",
+  },
+  {
+    title: "Python",
+    value: "python",
+  },
+  {
+    title: "PHP",
+    value: "php",
+  },
+  {
+    title: "Ruby",
+    value: "ruby",
+  },
+];
