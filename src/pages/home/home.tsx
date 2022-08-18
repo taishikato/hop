ce645ea60acb8851843d010d292b98803539d931
taishikato <taishi.k0903@gmail.com>
2022-08-17 20:39:45 -0700

    change button color

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,7 +3,7 @@ import { Link } from "@solidjs/router";
 const home = () => {
   return (
     <div class="flex justify-center mt-12">
-      <Link href="/app" class="btn btn-primary">
+      <Link href="/app" class="btn btn-secondary">
         See the jobs
       </Link>
     </div>
