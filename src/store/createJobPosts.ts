af7f3a559dd715b99a084c200b482868c92f2b1d
taishikato <taishi.k0903@gmail.com>
2022-08-20 13:31:20 -0700

    add isDone prop to jobPosts store

--- src/store/createJobPosts.ts
+++ src/store/createJobPosts.ts
@@ -2,7 +2,9 @@ import { createSignal, createRoot } from "solid-js";
 import type { JobPosts } from "../types/JobPosts";
 
 const createJobPosts = () => {
-  const [jobPosts, setJobPosts] = createSignal<JobPosts[]>([]);
+  const [jobPosts, setJobPosts] = createSignal<
+    { isDone: boolean; post: JobPosts }[]
+  >([]);
 
   return { jobPosts, setJobPosts };
 };
