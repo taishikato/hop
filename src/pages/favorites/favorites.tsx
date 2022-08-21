3a72b04f85905fdf26dd9ea93bce546bc624b1d1
taishikato <taishi.k0903@gmail.com>
2022-08-21 12:39:55 -0700

    show favorite list

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,8 +1,18 @@
 import { createEffect, createSignal } from "solid-js";
 import supabase from "../../supabaseClient";
 
+type FavoriteItem = {
+  startupjob_id: number;
+  startupjob_title: string;
+  startupjob_url: string;
+  startupjob_commitment: string;
+  startupjob_company_name: string;
+  startupjob_city: string | null;
+  startupjob_country: string | null;
+};
+
 const favorites = () => {
-  const [favoriteList, setFavoriteList] = createSignal([]);
+  const [favoriteList, setFavoriteList] = createSignal<FavoriteItem[]>([]);
 
   createEffect(async () => {
     const authUser = supabase.auth.user();
@@ -15,10 +25,24 @@ const favorites = () => {
     setFavoriteList(data);
   });
   return (
-    <div class="w-[700px] mx-auto border border-slate-800 rounded-lg p-3">
+    <div class="w-[90%] mx-auto md:w-[700px] border border-slate-800 rounded-lg p-3 space-y-6">
       {favoriteList().length > 0 &&
         favoriteList().map((item) => {
-          return <div>{JSON.stringify(item)}</div>;
+          return (
+            <a
+              class="p-3 hover:bg-slate-800 rounded-lg block cursor-pointer"
+              href={item.startupjob_url}
+              target="_blank"
+            >
+              <div>{item.startupjob_company_name}</div>
+              <div class="text-lg font-semibold">{item.startupjob_title}</div>
+              {item.startupjob_city && item.startupjob_country && (
+                <div class="badge badge-secondary">
+                  {item.startupjob_city} - {item.startupjob_country}
+                </div>
+              )}
+            </a>
+          );
         })}
     </div>
   );
