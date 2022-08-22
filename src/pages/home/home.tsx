e7d02531ad83c5edc85b04f98c76bfef8c1a5a96
taishikato <taishi.k0903@gmail.com>
2022-08-21 18:27:38 -0700

    responsive: lp

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,7 +3,7 @@ import { Link } from "@solidjs/router";
 const home = () => {
   return (
     <>
-      <div class="mt-12 w-[700px] mx-auto text-center mb-24">
+      <div class="mt-12 w-[90%] md:w-[700px] mx-auto text-center mb-24">
         <div class="text-5xl font-black text-center mb-7 bg-clip-text text-transparent bg-gradient-to-r from-pink-400  to-blue-400">
           {/* Your favorite app to find the best job  */}
           Let's find a partner of your life - a job that you love
