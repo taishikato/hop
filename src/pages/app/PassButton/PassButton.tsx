51090c9fe59928197db2e68582173a47606acc54
taishikato <taishi.k0903@gmail.com>
2022-08-25 19:26:27 -0700

    add a hover effect on PassButton

--- src/pages/app/PassButton/PassButton.tsx
+++ src/pages/app/PassButton/PassButton.tsx
@@ -1,6 +1,6 @@
 const PassButton = ({ handleFunction, forMobile = false }) => (
   <button
-    class={`p-5 rounded-full bg-slate-400 ${
+    class={`p-5 rounded-full bg-slate-400 focus:outline-none hover:bg-slate-500 ${
       !forMobile ? "hidden md:block" : ""
     }`}
     onClick={(e) => handleFunction(e)}
