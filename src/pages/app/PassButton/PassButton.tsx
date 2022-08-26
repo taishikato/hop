54a6ddf48674af3344f1592d4312cc9dc293b9fc
taishikato <taishi.k0903@gmail.com>
2022-08-25 19:25:28 -0700

    reafactor buttons

--- src/pages/app/PassButton/PassButton.tsx
+++ src/pages/app/PassButton/PassButton.tsx
@@ -1,16 +1,12 @@
-import { IoClose } from "solid-icons/io";
-
-const PassButton = ({ handleFunction, forMobile = false }) => {
-  return (
-    <button
-      class={`p-5 rounded-full bg-slate-400 ${
-        !forMobile ? "hidden md:block" : ""
-      }`}
-      onClick={(e) => handleFunction(e)}
-    >
-      <span class="block w-[24px] h-[24px]">👋</span>
-    </button>
-  );
-};
+const PassButton = ({ handleFunction, forMobile = false }) => (
+  <button
+    class={`p-5 rounded-full bg-slate-400 ${
+      !forMobile ? "hidden md:block" : ""
+    }`}
+    onClick={(e) => handleFunction(e)}
+  >
+    <span class="block w-[24px] h-[24px]">👋</span>
+  </button>
+);
 
 export default PassButton;
